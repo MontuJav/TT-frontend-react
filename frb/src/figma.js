0d9c0ae4d6e006a225cceaecd9cd13a3f67c6bae
javie <arbeit.mount@gmail.com>
2023-03-31 21:51:15 +0200

    added code generated

---
+++ frb/src/figma.js
@@ -0,0 +1,510 @@
+import React from 'react'
+
+import projectStyles from '.style.module.css'
+import styles from './mainview.module.css'
+
+const Mainview = (props) => {
+  return (
+    <div>
+      <div className={styles['mainview']}>
+        <div className={styles['frame17']}>
+          <div className={styles['card']}>
+            <img
+              src="/playground_assets/panel9212-65uo-300w.png"
+              alt="panel9212"
+              className={styles['panel']}
+            />
+            <img
+              src="/playground_assets/img19211-el3-200h.png"
+              alt="img19211"
+              className={styles['img1']}
+            />
+            <div className={styles['frame12']}>
+              <span
+                className={` ${styles['text']} ${projectStyles['normalm']} `}
+              >
+                <span>Alamacenamos tus datos de manera segura</span>
+              </span>
+              <span
+                className={` ${styles['text002']} ${projectStyles['normals']} `}
+              >
+                <span>
+                  Guardamos en nuestra base de datos de forma encriptada los
+                  datos sensibles de nuestros clientes
+                </span>
+              </span>
+              <div className={styles['knowmore']}>
+                <span
+                  className={` ${styles['text004']} ${projectStyles['normals']} `}
+                >
+                  <span>Dime mas</span>
+                </span>
+                <img
+                  src="/playground_assets/info12323-gvrq-200h.png"
+                  alt="info12323"
+                  className={styles['info1']}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles['card1']}>
+            <img
+              src="/playground_assets/panel2194-u0wu-300w.png"
+              alt="panel2194"
+              className={styles['panel1']}
+            />
+            <img
+              src="/playground_assets/img22194-ymgd-200h.png"
+              alt="img22194"
+              className={styles['img2']}
+            />
+            <div className={styles['frame121']}>
+              <span
+                className={` ${styles['text006']} ${projectStyles['normalm']} `}
+              >
+                <span>
+                  Ten a la mano los registros petroleros con los que cuentas
+                </span>
+              </span>
+              <span
+                className={` ${styles['text008']} ${projectStyles['normals']} `}
+              >
+                <span>
+                  Si requieres almacenar permanentemente la información de los
+                  registros en una base de datos, puedes almacenar hasta 3
+                  registros de forma gratuita
+                </span>
+              </span>
+              <div className={styles['knowmore1']}>
+                <span
+                  className={` ${styles['text010']} ${projectStyles['normals']} `}
+                >
+                  <span>Dime mas</span>
+                </span>
+                <img
+                  src="/playground_assets/info12323-wu1b-200h.png"
+                  alt="info12323"
+                  className={styles['info11']}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles['card2']}>
+            <img
+              src="/playground_assets/panel2194-wf-300w.png"
+              alt="panel2194"
+              className={styles['panel2']}
+            />
+            <img
+              src="/playground_assets/img32194-plz-200h.png"
+              alt="img32194"
+              className={styles['img3']}
+            />
+            <div className={styles['frame122']}>
+              <span
+                className={` ${styles['text012']} ${projectStyles['normalm']} `}
+              >
+                <span>
+                  Obten información de cada registro
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span
+                className={` ${styles['text014']} ${projectStyles['normals']} `}
+              >
+                <span>
+                  Obten información acerca de que parámetros estan siendo
+                  medidos en el registro, así como los rangos de los datos en
+                  los que se encuentran
+                </span>
+              </span>
+              <div className={styles['knowmore2']}>
+                <span
+                  className={` ${styles['text016']} ${projectStyles['normals']} `}
+                >
+                  <span>Dime mas</span>
+                </span>
+                <img
+                  src="/playground_assets/info12323-mc0g-200h.png"
+                  alt="info12323"
+                  className={styles['info12']}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles['card3']}>
+            <img
+              src="/playground_assets/panel2254-zaj8-300w.png"
+              alt="panel2254"
+              className={styles['panel3']}
+            />
+            <img
+              src="/playground_assets/img32254-ngim-200h.png"
+              alt="img32254"
+              className={styles['img31']}
+            />
+            <div className={styles['frame123']}>
+              <span
+                className={` ${styles['text018']} ${projectStyles['normalm']} `}
+              >
+                <span>Lorem ipsum dolor sit amet</span>
+              </span>
+              <span
+                className={` ${styles['text020']} ${projectStyles['normals']} `}
+              >
+                <span>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et
+                  massa mi. Aliquam in hendrerit urna. Ellentesque sit amet
+                  sapien fringilla, mattis ligula consectetur, ultrices mauris.
+                </span>
+              </span>
+              <div className={styles['knowmore3']}>
+                <span
+                  className={` ${styles['text022']} ${projectStyles['normals']} `}
+                >
+                  <span>Dime mas</span>
+                </span>
+                <img
+                  src="/playground_assets/info12323-n07-200h.png"
+                  alt="info12323"
+                  className={styles['info13']}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          src="/playground_assets/background5181-89sa-700h.png"
+          alt="background5181"
+          className={styles['background']}
+        />
+        <img
+          src="/playground_assets/layout2279-l1i-700h.png"
+          alt="layout2279"
+          className={styles['layout']}
+        />
+        <div className={styles['frametitle']}>
+          <span
+            className={` ${styles['text024']} ${projectStyles['titlewhite-n']} `}
+          >
+            <span>Gruppie</span>
+          </span>
+          <span className={styles['text026']}>
+            <span>
+              Diseñamos una herramienta que ayuda en el análisis de registros de
+              pozo petroleros, si tu también estas buscando una herramienta que
+              te permita clasificar tus registros de pozo por litologías
+              registrarte ya.
+            </span>
+          </span>
+          <button className={styles['button']}>
+            <img
+              src="/playground_assets/rectangle13i972-lell-200h.png"
+              alt="Rectangle13I972"
+              className={styles['rectangle13']}
+            />
+            <span className={styles['text028']}>
+              <span>Crear una cuenta</span>
+            </span>
+          </button>
+        </div>
+        <span
+          className={` ${styles['text030']} ${projectStyles['titlesubtitle-n']} `}
+        >
+          <span>
+            Acceso seguro a tu cuenta y almacenamiento seguro de tus datos
+          </span>
+        </span>
+        <div className={styles['frame18']}>
+          <img
+            src="/playground_assets/image19214-jhm6-300h.png"
+            alt="image19214"
+            className={styles['image1']}
+          />
+          <div className={styles['frame-desc']}>
+            <span
+              className={` ${styles['text032']} ${projectStyles['titlewhite-n']} `}
+            >
+              <span>
+                Una herramienta que apoya a los exper-tos en el análisis del
+                subsuelo
+              </span>
+            </span>
+            <span
+              className={` ${styles['text034']} ${projectStyles['normalm']} `}
+            >
+              <span>
+                Nuestra herramienta pretende ayudar a los expertos con la
+                identificación de zonas del subsuelo, así como de las litologías
+                que lo conforman
+              </span>
+            </span>
+            <button className={styles['button1']}>
+              <img
+                src="/playground_assets/rectangle13i921-47b-200h.png"
+                alt="Rectangle13I921"
+                className={styles['rectangle131']}
+              />
+              <span
+                className={` ${styles['text036']} ${projectStyles['normals']} `}
+              >
+                <span>Conocer más</span>
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className={styles['frame19']}>
+          <div className={styles['frame-desc1']}>
+            <span
+              className={` ${styles['text038']} ${projectStyles['titlewhite-n']} `}
+            >
+              <span>
+                Guarda los registros en una base de datos para respaldarlos
+              </span>
+            </span>
+            <span
+              className={` ${styles['text040']} ${projectStyles['normalm']} `}
+            >
+              <span>
+                Ya no necesitas cargar cada vez el mismo registro para su
+                análisis, ahora tú eliges que registros guardar y cuales
+                eliminar
+              </span>
+            </span>
+            <button className={styles['button2']}>
+              <img
+                src="/playground_assets/rectangle13i921-yild-200h.png"
+                alt="Rectangle13I921"
+                className={styles['rectangle132']}
+              />
+              <span
+                className={` ${styles['text042']} ${projectStyles['normals']} `}
+              >
+                <span>Conocer más</span>
+              </span>
+            </button>
+          </div>
+          <img
+            src="/playground_assets/image29216-4cw-300h.png"
+            alt="image29216"
+            className={styles['image2']}
+          />
+        </div>
+        <img
+          src="/playground_assets/rectangle239617-2pm9-500h.png"
+          alt="Rectangle239617"
+          className={styles['rectangle23']}
+        />
+        <div className={styles['explore']}>
+          <span className={styles['text044']}>
+            <span>Explore</span>
+          </span>
+          <div className={styles['fields']}>
+            <span className={styles['text046']}>
+              <span>lorem</span>
+            </span>
+            <span className={styles['text048']}>
+              <span>ipsum</span>
+            </span>
+            <span className={styles['text050']}>
+              <span>dolor</span>
+            </span>
+            <span className={styles['text052']}>
+              <span>sit amet</span>
+            </span>
+            <span className={styles['text054']}>
+              <span>consectetur</span>
+            </span>
+            <span className={styles['text056']}>
+              <span>
+                Worem ipsum dolor
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className={styles['company']}>
+          <span className={styles['text058']}>
+            <span>Company</span>
+          </span>
+          <div className={styles['fields1']}>
+            <span className={styles['text060']}>
+              <span>Porem ipsum dolor</span>
+            </span>
+            <span className={styles['text062']}>
+              <span> sit amet consectetur adipiscing elit</span>
+            </span>
+            <span className={styles['text064']}>
+              <span>Nunc vulputate libero</span>
+            </span>
+            <span className={styles['text066']}>
+              <span>et velit interdum</span>
+            </span>
+            <span className={styles['text068']}>
+              <span>ac aliquet odio mattis</span>
+            </span>
+            <span className={styles['text070']}>
+              <span>
+                Worem ipsum dolor
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className={styles['support']}>
+          <span className={styles['text072']}>
+            <span>Support</span>
+          </span>
+          <div className={styles['fields2']}>
+            <span className={styles['text074']}>
+              <span>Contact</span>
+            </span>
+            <span className={styles['text076']}>
+              <span>Help</span>
+            </span>
+            <span className={styles['text078']}>
+              <span>About Gruppie</span>
+            </span>
+            <span className={styles['text080']}>
+              <span>Careers</span>
+            </span>
+            <span className={styles['text082']}>
+              <span>Newsroom</span>
+            </span>
+            <span className={styles['text084']}>
+              <span>Whistleblower system</span>
+            </span>
+          </div>
+        </div>
+        <div className={styles['developer']}>
+          <span className={styles['text086']}>
+            <span>Developer &amp; IT</span>
+          </span>
+          <div className={styles['fields3']}>
+            <span className={styles['text088']}>
+              <span>Corem ipsum dolor sit amet</span>
+            </span>
+            <span className={styles['text090']}>
+              <span>Sectetur adipiscing elit</span>
+            </span>
+            <span className={styles['text092']}>
+              <span> Nunc vulputate libero et velit interdum</span>
+            </span>
+            <span className={styles['text094']}>
+              <span>Ac aliquet odio mattis class aptent</span>
+            </span>
+            <span className={styles['text096']}>
+              <span> taciti sociosqu ad litora</span>
+            </span>
+            <span className={styles['text098']}>
+              <span>Per conubia nostra</span>
+            </span>
+          </div>
+        </div>
+        <div className={styles['rights']}>
+          <span className={styles['text100']}>
+            <span>© 2023 Gruppie. All rights reserved.</span>
+          </span>
+          <div className={styles['frameservice']}>
+            <span className={styles['text102']}>
+              <span>Terms of service</span>
+            </span>
+            <span className={styles['text104']}>
+              <span>Privacy statement</span>
+            </span>
+            <span className={styles['text106']}>
+              <span>Do Not Sell or Share My Personal Information</span>
+            </span>
+            <span className={styles['text108']}>
+              <span>Cookie settings</span>
+            </span>
+            <span className={styles['text110']}>
+              <span>Interest based ads</span>
+            </span>
+            <span className={styles['text112']}>
+              <span>Recalls</span>
+            </span>
+          </div>
+        </div>
+        <img
+          src="/playground_assets/line19736-n66a.svg"
+          alt="Line19736"
+          className={styles['line1']}
+        />
+        <div className={styles['navbar']}>
+          <img
+            src="/playground_assets/headeri227-l3p-200h.png"
+            alt="headerI227"
+            className={styles['header']}
+          />
+          <div className={styles['gruppie-icon']}>
+            <img
+              src="/playground_assets/icongruppiei227-oxch-200h.png"
+              alt="ICONGRUPPIEI227"
+              className={styles['icongruppie']}
+            />
+            <span className={styles['text114']}>
+              <span>Gruppie</span>
+            </span>
+          </div>
+          <div className={styles['frame20']}>
+            <div className={styles['header1']}>
+              <div className={styles['frame10']}>
+                <span
+                  className={` ${styles['text116']} ${projectStyles['normalnavbar']} `}
+                >
+                  <span>Crear cuenta</span>
+                </span>
+              </div>
+            </div>
+            <div className={styles['droplist']}>
+              <img
+                src="/playground_assets/rectangle3i227-jtyl-200h.png"
+                alt="Rectangle3I227"
+                className={styles['rectangle3']}
+              />
+              <div className={styles['frame6']}>
+                <span
+                  className={` ${styles['text118']} ${projectStyles['normalnavbar']} `}
+                >
+                  <span>Inicio de sesión</span>
+                </span>
+                <img
+                  src="/playground_assets/logini227-6g5q-200w.png"
+                  alt="loginI227"
+                  className={styles['login']}
+                />
+              </div>
+            </div>
+            <div className={styles['header2']}>
+              <span
+                className={` ${styles['text120']} ${projectStyles['normalnavbar']} `}
+              >
+                <span>Busqueda</span>
+              </span>
+              <img
+                src="/playground_assets/search3i227-8b6w-200h.png"
+                alt="search3I227"
+                className={styles['search3']}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Mainview
